116858c6b2347650de6089e9a680d344a1c31f75
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-03-21 00:21:07 +0300

    ft: this commit added user interface logic

--- main.js
+++ main.js
@@ -4,4 +4,28 @@ function Address(streetAddress, city, state) {
     this.city = city;
     this.state = state;
     this.deliveryAddress = (streetAddress + "  " + city + ", " + state);
-}
+}//User Interface Logic
+$(document).ready(function(event) {
+    $("#pickup-btn").click(function() {
+        $("#order-content").show();
+        $("#landing-content").hide();
+        $("#delivery-option").text("PICKUP BY CUSTOMER");
+    });
+    $("#delivery-btn").click(function() {
+        $("#address").show();
+        $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
+    });
+    $("form#address-form").submit(function(event) {
+        event.preventDefault();
+        var streetAddress = $("input#street-add").val();
+        var city = $("input#city-add").val();
+        var state = $("input#state-select").val();
+        // var zipcode = $("input#zip-add").val();
+        var newAddress = new Address(streetAddress, city, state)
+        $("#order-content").show();
+        $("#landing-content").hide();
+        $("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
+    });
+    $("#sides-dropdown").click(function() {
+        $("#sides-details").toggle();
+    });
